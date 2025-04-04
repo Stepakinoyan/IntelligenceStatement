2cb77f4e8bf27b3c9f1565cf558f104166d989da
ScrollDude <stepakinoyan@gmail.com>
2025-04-04 05:50:01 +0300

    fix(docker): fix ports

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -10,11 +10,11 @@ export interface DocumentAnalysisData {
 }
 
 export function useApi() {
-  const apiUrl = "http://localhost:8000/";
+  const apiUrl = "http://localhost:5555/";
 
   const fetchRecentDocuments = async (): Promise<DocumentPreview[]> => {
     try {
-      const response = await axios.get(`http://localhost:8000/pdf/alls`);
+      const response = await axios.get(`http://localhost:5555/pdf/alls`);
       return response.data;
     } catch (error) {
       console.error("Error fetching document analysis:", error);
