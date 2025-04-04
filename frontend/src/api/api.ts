c4a943609fa2e4de5e79284d0a898e1beb353a66
ScrollDude <stepakinoyan@gmail.com>
2025-04-04 05:33:38 +0300

    fix(docker): fix docker-compose.yml

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -10,11 +10,11 @@ export interface DocumentAnalysisData {
 }
 
 export function useApi() {
-  const apiUrl = import.meta.env.VITE_API_URL;
+  const apiUrl = "http://localhost:8000/";
 
   const fetchRecentDocuments = async (): Promise<DocumentPreview[]> => {
     try {
-      const response = await axios.get(`${apiUrl}/pdf/all`);
+      const response = await axios.get(`http://localhost:8000/pdf/alls`);
       return response.data;
     } catch (error) {
       console.error("Error fetching document analysis:", error);
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "./scss/styles.scss";
-import * as bootstrap from "bootstrap";
+// import * as bootstrap from "bootstrap";
 
 const app = createApp(App);
 
